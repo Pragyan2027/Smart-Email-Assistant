d11c4a1fbd139b2509b31ff9675679f35e322aac
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-07-04 07:02:39 +0530

    Update GenerateReplyController.java

--- src/main/java/com/email/writer/app/GenerateReplyController.java
+++ src/main/java/com/email/writer/app/GenerateReplyController.java
@@ -1,4 +1,21 @@
+
 package com.email.writer.app;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
 public class GenerateReplyController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generateReply")
+    public Mono<ResponseEntity<String>> generateReply(@RequestBody EmailRequest emailRequest) {
+        return emailGeneratorService.generateEmailReply(emailRequest)
+                .map(ResponseEntity::ok);
+    }
 }
