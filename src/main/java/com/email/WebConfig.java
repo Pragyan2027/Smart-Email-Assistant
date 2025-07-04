f8b9e382d4f1da6c76475fd811a0a2048755b81d
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-07-04 07:04:09 +0530

    Update WebConfig.java

--- src/main/java/com/email/WebConfig.java
+++ src/main/java/com/email/WebConfig.java
@@ -1,4 +1,19 @@
+
 package com.email;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://mail.google.com")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
+
